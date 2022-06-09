3ea74c30177a373d93e0df2a972068e671cc8df3
Felix Billon <felix.billon@wanadoo.fr>
2022-06-09 14:43:30 +0200

    Update vector.service.ts (#36)

--- src/vector/vector.service.ts
+++ src/vector/vector.service.ts
@@ -4,6 +4,7 @@ import { Vector } from "./vector";
 @Injectable()
 export class VectorService {
   round(vector: Vector): Vector {
+    console.log(eval("vector.x + vector.y"));
     return {
       x: Math.round(vector.x),
       y: Math.round(vector.y),
